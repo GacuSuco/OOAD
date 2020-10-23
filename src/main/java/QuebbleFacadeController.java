3f18a5c1d69441b9c8ee0d562ce189d4bea6ba37
Mitch peters <M.peters12@student.han.nl>
2020-10-23 19:33:11 +0200

    init commit

---
+++ src/main/java/QuebbleFacadeController.java
@@ -0,0 +1,8 @@
+public class QuebbleFacadeController {
+    public void startQuiz(){}
+    public void getNextQuestion(){}
+    public void answerQuestion(String answer){}
+    public void startCheckword(){}
+    public void answerCheckword(String answer){}
+    public void endQuiz(){}
+}
